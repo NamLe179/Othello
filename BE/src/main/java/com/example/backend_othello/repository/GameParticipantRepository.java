8cb7d8a4cb9a1e42af3455a896b60b14190a82ad
NamLe179 <ldnam179@gmail.com>
2025-05-26 02:25:51 +0700

    update BXH

--- BE/src/main/java/com/example/backend_othello/repository/GameParticipantRepository.java
+++ BE/src/main/java/com/example/backend_othello/repository/GameParticipantRepository.java
@@ -13,16 +13,16 @@ import java.util.List;
 
 @Repository
 public interface GameParticipantRepository extends JpaRepository<GameParticipant, String> {
-    @Query("SELECT gp FROM GameParticipant gp JOIN gp.game g WHERE g.id = :gameId")
-    GameParticipant findByGameId(@Param("gameId") String gameId);
-
-    GameParticipant findByGameIdAndUserId(String gameId, String userId);
+//    @Query("SELECT gp FROM GameParticipant gp JOIN gp.game g WHERE g.id = :gameId")
+//    GameParticipant findByGameId(@Param("gameId") String gameId);
+//
+//    GameParticipant findByGameIdAndUserId(String gameId, String userId);
 
     //Truy vấn xếp hạng người chơi theo số trận thắng, thời gian
     //Tạo ds TKUser từ truy vấn với thông tin User từ bảng User, input và hàm đếm
     @Query("SELECT new com.example.backend_othello.entity.TKUser(u.id, u.userName, u.email, :startDate, :endDate, :gameType, COUNT(gp)) " +
-            "FROM com.example.backend_othello.entity.GameParticipant gp " +
-            "JOIN gp.game g " +
+            "FROM GameParticipant gp " +
+            "JOIN Game g ON g.gameParticipant.id = gp.id " +
             "JOIN gp.user u " +
             "WHERE gp.result = 'WIN' " +
             "AND g.gameType = :gameType " +
